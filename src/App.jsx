fdf3f7dda04afb95194360c69d61d285c71df997
Your NameFeisal14 <fisalgemar@gmail.com>
2024-08-04 15:07:53 +0300

    Added the slider auto animation for entities in home page

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import HomePage from "./Pages/HomePage";
 import ErrorPage from "./Pages/ErrorPage";
+import Entites from '../src/Home/EntittesComponents/Entites'
 function App() {
   return (
     <BrowserRouter>
@@ -8,6 +9,7 @@ function App() {
         <Route index element={<HomePage />} />
         <Route path="*" element={<ErrorPage />} />
       </Routes>
+      <Entites/>
     </BrowserRouter>
   );
 }
