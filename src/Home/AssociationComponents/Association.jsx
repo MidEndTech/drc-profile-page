4c3846ba9abc44d7ff5379eb7439dff76d191b38
Feisal14 <fisalgemar@gmail.com>
2024-08-06 14:04:25 +0300

    did the componnets and waiting for review

--- src/Home/AssociationComponents/Association.jsx
+++ src/Home/AssociationComponents/Association.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import Slider from "react-slick";
 import { Images } from "./images.json";
 import "slick-carousel/slick/slick.css";
@@ -6,28 +6,49 @@ import "slick-carousel/slick/slick-theme.css";
 import left from "../../../public/assets/Home/AssociatationImages/Variant5.svg";
 import right from "../../../public/assets/Home/AssociatationImages/Variant6.svg";
 
-const NextArrow = ({ onClick }) => (
-  <div onClick={onClick} className="custom-arrow next-arrow p-2">
-    <img src={right} alt="Next" className="w-8 h-8 md:w-10 md:h-10 lg:w-12 lg:h-12" />
+const NextArrow = ({ onClick, disabled }) => (
+  <div
+    onClick={onClick}
+    className={`custom-arrow next-arrow p-2 ${
+      disabled ? "opacity-50 cursor-not-allowed" : ""
+    }`}
+  >
+    <img
+      src={left}
+      alt="Next"
+      className="w-8 h-8 md:w-10 md:h-10 lg:w-12 lg:h-12"
+    />
   </div>
 );
 
-const PrevArrow = ({ onClick }) => (
-  <div onClick={onClick} className="custom-arrow prev-arrow p-2">
-    <img src={left} alt="Previous" className="w-8 h-8 md:w-10 md:h-10 lg:w-12 lg:h-12" />
+const PrevArrow = ({ onClick, disabled }) => (
+  <div
+    onClick={onClick}
+    className={`custom-arrow prev-arrow p-2 ${
+      disabled ? "opacity-50 cursor-not-allowed" : ""
+    }`}
+  >
+    <img
+      src={right}
+      alt="Previous"
+      className="w-8 h-8 md:w-10 md:h-10 lg:w-12 lg:h-12"
+    />
   </div>
 );
 
-function AssociationCarousel()  {
+function AssociationCarousel() {
   const allImages = Images.flatMap((img) => img.src);
 
+  const [currentSlide, setCurrentSlide] = useState(0);
+
   const settings = {
-    infinite: true,
+    infinite: false,
     speed: 500,
     slidesToShow: 4,
-    slidesToScroll: 4,
+    slidesToScroll: 3,
     rows: 3,
-    arrows: false, 
+    arrows: false,
+    afterChange: (current) => setCurrentSlide(current),
     responsive: [
       {
         breakpoint: 1024,
@@ -59,7 +80,7 @@ function AssociationCarousel()  {
   let sliderRef = React.createRef();
 
   return (
-    <div className="container mx-auto px-4">
+    <div className="container mx-auto px-1">
       <h1 className="text-2xl sm:text-3xl md:text-4xl text-[#9D6636] font-bold text-center my-4">
         شركاء النجاح
       </h1>
@@ -69,21 +90,26 @@ function AssociationCarousel()  {
             <img
               src={src}
               alt={`Logo ${index + 1}`}
-              className="h-12 sm:h-14 md:h-16 lg:h-18 xl:h-20 object-contain mx-auto my-2"
+              className="h-10 sm:h-12 md:h-14 lg:h-16 xl:h-18 object-contain px-4 sm:px-6 md:px-8 lg:px-10"
             />
           </div>
         ))}
       </Slider>
       <div className="flex justify-center mt-8 space-x-8">
         <div className="flex items-center justify-center">
-          <PrevArrow onClick={() => sliderRef.slickPrev()} />
+          <NextArrow
+            onClick={() => sliderRef.slickNext()}
+            disabled={currentSlide >= allImages.length - 4}
+          />
         </div>
         <div className="flex items-center justify-center">
-          <NextArrow onClick={() => sliderRef.slickNext()} />
+          <PrevArrow
+            onClick={() => sliderRef.slickPrev()}
+            disabled={currentSlide === 0}
+          />
         </div>
       </div>
     </div>
   );
-};
-
+}
 export default AssociationCarousel;
--- src/Home/EntittesComponents/Entites.jsx
+++ src/Home/EntittesComponents/Entites.jsx
@@ -1,13 +1,12 @@
-import './autoSlider.css'
+import './autoSlider.css';
 import { Images } from "./images.json";
 
-
-
-
 function Entites() {
-    const allImages = Images.Images.flatMap(img => img.src); 
+    
+    const allImages = Images[0].src;
+
     return (
-      <div className="container mx-auto px-4">
+      <div className="container mx-auto px-8">
         <h1 className="text-2xl sm:text-3xl md:text-4xl text-[#9D6636] font-bold text-center my-4 pb-10">جهات معتمدة لديها دار الرحمة للاستشارات</h1>
         <div className="overflow-hidden">
           <div className="flex animate-scroll">
@@ -16,7 +15,6 @@ function Entites() {
                 key={index} 
                 src={src} 
                 alt={`img ${index + 1}`} 
-
                 className="h-10 sm:h-11 md:h-13 lg:h-15 xl:h-17 object-contain px-4 sm:px-6 md:px-8 lg:px-10"
               />
             ))}
