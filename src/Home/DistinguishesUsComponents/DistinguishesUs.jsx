b83f1ab7fcde779ccebfb5c4b34786e2dce90e77
Musab Alsayed <musab20151@gmail.com>
2024-08-07 07:08:17 +0300

    just fitch it too My Laptop

--- src/Home/DistinguishesUsComponents/DistinguishesUs.jsx
+++ src/Home/DistinguishesUsComponents/DistinguishesUs.jsx
@@ -2,8 +2,21 @@ import React, { useState } from "react";
 import LogoUs from "/assets/DistinguishesUsSection/LogoUs.svg";
 import BackgroundOrangeColor from "/assets/DistinguishesUsSection/BackgroundOrangeColor.svg";
 
+//import JSON files
+import LensesAndDimensionObj from "./LensesAndDimension.json";
+import featuresObj from "./features.json";
+import internalValuesObj from "./internalValues.json";
+
 function DistinguishesUs() {
   const [fog, setFog] = useState(null);
+
+  const [lensesAndDimension, setLensesAndDimension] = useState([
+    ...LensesAndDimensionObj,
+  ]);
+
+  const [features, setFeatures] = useState([...featuresObj]);
+
+  const [internalValues, setInternalValues] = useState([...internalValuesObj]);
 
   const lensesAndDimensionsHandle = () => {
     setFog("LensesAnd_dimensions");
@@ -20,11 +33,11 @@ function DistinguishesUs() {
   const fogHandle = () => {
     switch (fog) {
       case "LensesAnd_dimensions":
-        return "blur-sm";
+        return "LensesAnd_dimensions";
       case "features":
-        return "blur-md";
+        return "features";
       case "Internal_values":
-        return "blur-lg";
+        return "Internal_values";
       default:
         return "";
     }
@@ -33,7 +46,6 @@ function DistinguishesUs() {
   return (
     <div className="flex flex-col h-screen justify-center items-center">
       <h1 className="font-[700] text-[36px] text-[#9D6636] z-10">مايميزنا</h1>
-
 
       <div className="z-10 font-[600] text-[22px] md:font-[600] md:text-[28px] flex flex-row justify-around mt-10">
         <button
@@ -56,24 +68,51 @@ function DistinguishesUs() {
         </button>
       </div>
 
-      <div  onClick={() => setFog(null)} className={`${fogHandle()} relative shadow-2xl w-[350px] h-[350px] md:w-[415.91px] md:h-[415.91px] border-[11px] rounded-full mt-10 flex justify-center items-center`}>
+      <div
+        onClick={() => setFog(null)}
+        className={`${fogHandle()} z-20 relative shadow-2xl w-[350px] h-[350px] md:w-[415.91px] md:h-[415.91px] border-[11px] rounded-full mt-10 flex justify-center items-center bg-white`}
+      >
         <img src={LogoUs} alt="" />
-
-        
       </div>
 
-      <div className="mt-10 rotate-[4.4deg] shadow-xl border-1 rounded-full flex justify-center items-center bg-[#2C4D51]/10 py-[6px] px-[20px] absolute lg:left-[3.45px] lg:top-[202.22px]">
-                <p className="text-[#2C4D51]">
-                    الهوية
-                </p>
-      </div>
+      {fogHandle() === "LensesAnd_dimensions"
+        ? lensesAndDimension.map((el, index) => {
+            return (
+              <div
+                key={index}
+                className={`animate-bounce z-10 rotate-[${el.rotation}] shadow-xl border-1 rounded-full flex justify-center items-center bg-[${el.backGroundColor}]/10 py-[6px] px-[20px] absolute top-[${el.top}] left-[${el.left}]`}
+              >
+                <p className={`text-[${el.textColor}]`}>{el.text}</p>
+              </div>
+            );
+          })
+        : null}
 
-      
-      <div className="mt-10 rotate-[6.35deg] shadow-xl border-1 rounded-full flex justify-center items-center bg-[#BE894A]/10 py-[6px] px-[20px] absolute lg:left-[144.55px] right-[17%] lg:right-auto lg:top-[283px]">
-                <p className="text-[#BE894A]">
-                    فئة العميل  
-                </p>
-      </div> 
+      {fogHandle() === "features"
+        ? features.map((el, index) => {
+            return (
+              <div
+                key={index}
+                className={`animate-bounce z-10 rotate-[${el.rotation}] shadow-xl border-1 rounded-full flex justify-center items-center bg-[${el.backGroundColor}]/10 py-[6px] px-[20px] absolute top-[${el.top}] left-[${el.left}]`}
+              >
+                <p className={`text-[${el.textColor}]`}>{el.text}</p>
+              </div>
+            );
+          })
+        : null}
+
+      {fogHandle() === "Internal_values"
+        ? internalValues.map((el, index) => {
+            return (
+              <div
+                key={index}
+                className={`animate-bounce z-40 rotate-[${el.rotation}] shadow-xl border-1 rounded-full flex justify-center items-center bg-[${el.backGroundColor}]/10 py-[6px] px-[20px] absolute top-[${el.top}] left-[${el.left}]`}
+              >
+                <p className={`text-[${el.textColor}]`}>{el.text}</p>
+              </div>
+            );
+          })
+        : null}
 
       <div className="absolute left-0 z-0">
         <img src={BackgroundOrangeColor} alt="" />
