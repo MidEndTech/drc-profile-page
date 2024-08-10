41b956f63bf0353701f4ed4ad469ff0106d1be4c
Musab Alsayed <musab20151@gmail.com>
2024-08-11 01:29:13 +0300

    for sending to my labtob

--- src/Home/DistinguishesUsComponents/DistinguishesUs.jsx
+++ src/Home/DistinguishesUsComponents/DistinguishesUs.jsx
@@ -1,54 +1,56 @@
 import React, { useState } from "react";
 import LogoUs from "/assets/Home/DistinguishesUsSection/LogoUs.svg";
 import BackgroundOrangeColor from "/assets/Home/DistinguishesUsSection/BackgroundOrangeColor.svg";
+import gsap from "gsap";
 
 //import JSON files
 import LensesAndDimensionObj from "./LensesAndDimension.json";
-import featuresObj from "./Features.json";
-import internalValuesObj from "./InternalValues.json";
+import featuresObj from "./features.json";
+import internalValuesObj from "./internalValues.json";
 
 function DistinguishesUs() {
-  const [fog, setFog] = useState(null);
 
-  const [lensesAndDimension, setLensesAndDimension] = useState([
-    ...LensesAndDimensionObj,
-  ]);
-
+  const [cards, setCards] = useState(null);
+  const [lensesAndDimension, setLensesAndDimension] = useState([...LensesAndDimensionObj,]);
   const [features, setFeatures] = useState([...featuresObj]);
-
   const [internalValues, setInternalValues] = useState([...internalValuesObj]);
 
   const lensesAndDimensionsHandle = () => {
-    setFog("LensesAnd_dimensions");
+    setCards("LensesAnd_dimensions");
   };
 
   const featuresHandle = () => {
-    setFog("features");
+    setCards("features");
   };
 
   const internalValuesHandle = () => {
-    setFog("Internal_values");
+    setCards("Internal_values");
   };
 
-  const fogHandle = () => {
-    switch (fog) {
+  const cardsHandle = () => {
+    switch (cards) {
       case "LensesAnd_dimensions":
         return "LensesAnd_dimensions";
       case "features":
         return "features";
       case "Internal_values":
         return "Internal_values";
-      default:
-        return "";
     }
   };
 
+  // const responsivePageHandle = () => {
+  //       if(){
+  //         return lgLeft
+  //       }
+  // }
+
   return (
     <div className="flex flex-col h-screen justify-center items-center">
-      <h1 className="font-[700] text-[36px] text-[#9D6636] z-10">مايميزنا</h1>
+      <h1 className="font-[700] text-[36px] text-[#9D6636] mb-10">مايميزنا</h1>
 
-      <div className="h-[718.51px] w-[1142.89px] flex flex-col justify-center items-center relative mt-[-90px]">
-        <div className="z-10 font-[400] text-[22px] md:font-[400] md:text-[28px] flex flex-row justify-around">
+      <div className="lg:h-[718.51px] lg:w-[1142.89px] h-full flex flex-col justify-start lg:justify-center items-center relative lg:mt-[-90px] ">
+        {/*nead break it *****DONE****************************************************************************************************************************************************************************************/}
+        <div className="z-10 font-[400] text-[22px] md:font-[400] md:text-[28px]">
           <button
             onClick={() => lensesAndDimensionsHandle()}
             className="ml-10 text-[#8080808C] focus:text-[#BE894A] hover:text-[#BE894A]"
@@ -69,24 +71,30 @@ function DistinguishesUs() {
           </button>
         </div>
 
+        {/*nead break it *******Done**************************************************************************************************************************************************************************************/}
         <div
-          onClick={() => setFog(null)}
-          className={`${fogHandle()}z-20 relative shadow-2xl w-[350px] h-[350px] md:w-[415.91px] md:h-[415.91px] border-[11px] rounded-full mt-[50px] flex justify-center items-center bg-white`}
+          className={` relative shadow-lg w-[190px] h-[190px] md:w-[415.91px] md:h-[415.91px] border-[11px] rounded-full mt-[50px] flex justify-center items-center bg-white`}
         >
-          <img src={LogoUs} alt="" />
+          <img
+            src={LogoUs}
+            alt="شعار دار الرحمة"
+            className="w-[100px] h-[200px] md:w-[278.62px] md:h-[172.95px] "
+          />
         </div>
 
-        {fogHandle() === "LensesAnd_dimensions"
+        {/*nead break it *********************************************************************************************************************************************************************************************/}
+        {cardsHandle() === "LensesAnd_dimensions"
           ? lensesAndDimension.map((el, index) => {
               return (
                 <div
                   key={index}
-                  className={`animate-bounce z-10 shadow-xl border-1 rounded-full flex justify-center items-center py-[6px] px-[20px] absolute`}
+                  className={`animate-bounce z-10 shadow-md border-1 rounded-full flex justify-center items-center py-[6px] px-[20px] absolute`}
                   style={{
                     backgroundColor: `${el.backGroundColor}`,
-                    top: `${el.top}`,
-                    left: `${el.left}`,
+                    top: `${el.lgTop}px`,
+                    left: `${el.lgLeft}px`,
                     rotate: `${el.rotation}`,
+                    animationDuration: `${el.duration}s`,
                   }}
                 >
                   <p style={{ color: `${el.textColor}` }}>{el.text}</p>
@@ -95,17 +103,18 @@ function DistinguishesUs() {
             })
           : null}
 
-        {fogHandle() === "features"
+        {cardsHandle() === "features"
           ? features.map((el, index) => {
               return (
                 <div
                   key={index}
-                  className={`animate-bounce z-10 shadow-xl border-1 rounded-full flex justify-center items-center py-[6px] px-[20px] absolute`}
+                  className={`animate-bounce z-10 shadow-md border-1 rounded-full flex justify-center items-center py-[6px] px-[20px] absolute`}
                   style={{
                     backgroundColor: `${el.backGroundColor}`,
-                    top: `${el.top}`,
-                    left: `${el.left}`,
+                    top: `${el.lgTop}px`,
+                    left: `${el.lgLeft}px`,
                     rotate: `${el.rotation}`,
+                    animationDuration: `${el.duration}s`,
                   }}
                 >
                   <p style={{ color: `${el.textColor}` }}>{el.text}</p>
@@ -114,17 +123,18 @@ function DistinguishesUs() {
             })
           : null}
 
-        {fogHandle() === "Internal_values"
+        {cardsHandle() === "Internal_values"
           ? internalValues.map((el, index) => {
               return (
                 <div
                   key={index}
-                  className={`k z-10 shadow-xl border-1 rounded-full py-[6px] px-[20px] absolute`}
+                  className={`animate-bounce z-10 shadow-md border-1 rounded-full py-[6px] px-[20px] absolute md`}
                   style={{
                     backgroundColor: `${el.backGroundColor}`,
-                    top: `${el.top}`,
-                    left: `${el.left}`, 
+                    top: `${el.lgTop}px`,
+                    left: `${el.lgLeft}px`,
                     rotate: `${el.rotation}`,
+                    animationDuration: `${el.duration}s`,
                   }}
                 >
                   <p style={{ color: `${el.textColor}` }}>{el.text}</p>
@@ -132,12 +142,8 @@ function DistinguishesUs() {
               );
             })
           : null}
-
-
-
-
-
       </div>
+      {/*nead break it *********************************************************************************************************************************************************************************************/}
 
       <div className="absolute left-0 z-0">
         <img src={BackgroundOrangeColor} alt="" />
