60b6aa67c26ed08aaf348add9b06efc3803ca04a
Omir <omirabbasnw@gmail.com>
2024-08-11 16:13:38 +0300

    Create About Us Section in About Us Page

--- src/Pages/AboutUsPage.jsx
+++ src/Pages/AboutUsPage.jsx
@@ -1,5 +1,11 @@
+import AboutSection from "../AboutUs/HeroComponents/AboutSection";
+
 function AboutUsPage() {
-  return <div></div>;
+  return (
+    <div className="bg-[#F5F5F5]">
+      <AboutSection />
+    </div>
+  );
 }
 
 export default AboutUsPage;
