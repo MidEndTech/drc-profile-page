3071faefea0ae9d571ab32b89af4ad2b999fca5c
Feisal14 <fisalgemar@gmail.com>
2024-08-11 15:26:31 +0300

    switched to mui

--- src/AboutUs/FounderComponents/FounderHistory.jsx
+++ src/AboutUs/FounderComponents/FounderHistory.jsx
@@ -1,34 +1,82 @@
 import React from "react";
+import Timeline from "@mui/lab/Timeline";
+import TimelineItem from "@mui/lab/TimelineItem";
+import TimelineSeparator from "@mui/lab/TimelineSeparator";
+import TimelineConnector from "@mui/lab/TimelineConnector";
+import TimelineContent from "@mui/lab/TimelineContent";
+import TimelineOppositeContent from "@mui/lab/TimelineOppositeContent";
+import { Typography } from "@mui/material";
+import { styled } from "@mui/system";
+
+const CustomTimelineDot = styled("div")({
+  display: "flex",
+  justifyContent: "center",
+  alignItems: "center",
+  border: "2px solid #BE894A",
+  borderRadius: "50%",
+  width: "20px",
+  height: "20px",
+  backgroundColor: "white",
+  zIndex: 1,
+  "&::before": {
+    content: '""',
+    display: "block",
+    width: "10px",
+    height: "10px",
+    backgroundColor: "#BE894A",
+    borderRadius: "50%",
+  },
+});
+
+const CustomTimelineConnector = styled(TimelineConnector)(({ theme }) => ({
+  backgroundColor: "#2C4D51",
+}));
 
 function FounderHistory() {
+  const historyItems = [
+    {
+      date: "عام 1965م/1385هـ",
+      content:
+        "حصل على البكالوريوس الهندسة المعمارية من جامعة عين شمس بجمهورية مصر العربية",
+    },
+    {
+      date: "عام 1971م/1390هـ",
+      content:
+        "حصل على ماجستير تخطيط المدن من جامعة جنوب كاليفورنيا لوس أنجلوس أمريكا",
+    },
+    {
+      date: "عام 1980م/1400هـ",
+      content: "أسس مكتب التخطيط المعماري والهندسي",
+    },
+    {
+      date: "عام 1985م/1405هـ",
+      content: "عمل كمستشار في مشاريع التنمية الحضرية في المنطقة",
+    },
+    // Add more items as needed
+  ];
+
   return (
-    <div className="flex flex-col items-start">
-      {/* Line */}
-      <div className=" h-64 w-[1px] bg-black relative z-0">
-        {/* Circle */}
-        <div className="absolute left-0 right-0 flex justify-center z-10">
-          <div className="border-2 border-primary rounded-full p-1 ">
-            <div className="p-2 bg-primary rounded-full"></div>
-          </div>
-        </div>
-        <div className="absolute top-1/2 left-0 right-0 flex justify-center z-10">
-          <div className="border-2 border-primary rounded-full p-1 ">
-            <div className="p-2 bg-primary rounded-full"></div>
-          </div>
-        </div>
-        <div className="absolute top-1/4 left-0 right-0 flex justify-center z-10">
-          <div className="border-2 border-primary rounded-full p-1 ">
-            <div className="p-2 bg-primary rounded-full"></div>
-          </div>
-        </div>
-        <div className="absolute top-7 left-0 right-0 flex justify-center z-10">
-          <div className="border-2 border-primary rounded-full p-1 ">
-            <div className="p-2 bg-primary rounded-full"></div>
-          </div>
-        </div>
-      </div>
-    </div>
+    <Timeline position="right">
+      {historyItems.map((item, index) => (
+        <TimelineItem key={index}>
+          <TimelineOppositeContent
+            style={{ flex: 0.2, paddingLeft: 16 }}
+            align="left"
+            variant="body2"
+            color="text.secondary"
+          >
+            {item.date}
+          </TimelineOppositeContent>
+          <TimelineSeparator>
+            <CustomTimelineDot />   
+            {index < historyItems.length - 1 && <CustomTimelineConnector />}
+          </TimelineSeparator>
+          <TimelineContent style={{ paddingRight: 16 }}>
+            <Typography variant="body1">{item.content}</Typography>
+          </TimelineContent>
+        </TimelineItem>
+      ))}
+    </Timeline>
   );
 }
-
 export default FounderHistory;
