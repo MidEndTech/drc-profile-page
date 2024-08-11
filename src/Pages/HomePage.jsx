cd37b17351c84c7f5269e9e3f37031768bc5301b
Omir <omirabbasnw@gmail.com>
2024-08-11 09:10:00 +0300

    fix bugs

--- src/Pages/HomePage.jsx
+++ src/Pages/HomePage.jsx
@@ -1,16 +1,19 @@
 import HeroSection from "../Home/HeroComponents/HeroSection";
 import About from "../../src/Home/AboutComponents/About";
-import Entites from '../../src/Home/EntittesComponents/Entites'
-import Association from "../../src/Home/AssociationComponents/Association"
+import Entites from "../../src/Home/EntittesComponents/Entites";
+import Association from "../../src/Home/AssociationComponents/Association";
+import Projects from "../Home/ProjectsComponents/Projects";
 
 function HomePage() {
-  return <div>
-    <HeroSection />
-    <About/>
-    <Projects/>
-    <Association/>
-    <Entites/>
-  </div>;
+  return (
+    <div>
+      <HeroSection />
+      <About />
+      <Projects />
+      <Association />
+      <Entites />
+    </div>
+  );
 }
 
 export default HomePage;
