b3d19890dbc6c91a6bf9c840a7a3578a3bcd96ae
Feisal14 <fisalgemar@gmail.com>
2024-08-12 15:12:18 +0300

    Still working on the grid system

--- src/Pages/ProjectsPage.jsx
+++ src/Pages/ProjectsPage.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
 import HeroSection from '../Projects/HeroComponents/HeroSection'
+import Network from '../Projects/NetwrokSection/Network'
 
 function ProjectsPage() {
   return (
     <div>
-        <HeroSection/>  
+      <Network/>
     </div>
   )
 }
