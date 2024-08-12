5c51bd36fed0f8d7b7fdd716b2ce5e471939e566
Feisal14 <fisalgemar@gmail.com>
2024-08-12 10:53:34 +0300

    Moved Founder to aboutUs file

--- src/Pages/AboutUsPage.jsx
+++ src/Pages/AboutUsPage.jsx
@@ -1,10 +1,11 @@
-
+import Founder from "../AboutUs/FounderComponents/Founder";
 import AboutSection from "../AboutUs/HeroComponents/AboutSection";
 
 function AboutUsPage() {
   return (
     <div className="bg-[#F5F5F5]">
       <AboutSection />
+      <Founder />
     </div>
   );
 }
