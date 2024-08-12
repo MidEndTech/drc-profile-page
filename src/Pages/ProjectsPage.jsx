54180422b8339aaa9af3653e3098177398bc3f18
Feisal14 <fisalgemar@gmail.com>
2024-08-12 11:46:16 +0300

    Did the hero section of projects page

---
+++ src/Pages/ProjectsPage.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import HeroSection from '../Projects/HeroComponents/HeroSection'
+
+function ProjectsPage() {
+  return (
+    <div>
+        <HeroSection/>  
+    </div>
+  )
+}
+
+export default ProjectsPage
