7d1f19e54db200425f4f85749cb47ce66fa07ed4
Omir <omirabbasnw@gmail.com>
2024-08-13 21:22:59 +0300

    Create Hero Section in Services Page

--- src/Services/HeroComp/HeroSection.jsx
+++ src/Services/HeroComp/HeroSection.jsx
@@ -1,7 +1,7 @@
 import { useEffect, useRef, useState } from "react";
 
 function HeroSection() {
-  const [isText, setIsText] = useState(true);
+  const [isText, setIsText] = useState(false);
   const timerRef = useRef(null);
   useEffect(() => {
     if (timerRef.current) {
@@ -9,12 +9,21 @@ function HeroSection() {
     }
     timerRef.current = setTimeout(() => {
       setIsText(!isText);
-    }, 3000);
+    }, 2000);
     return () => clearTimeout(timerRef.current);
   });
   return (
     <div className="overflow-hidden relative">
       <div className="w-screen h-screen flex items-center justify-center ">
+        <section className="text-center mb-12 z-20">
+          <h1 className="text-primary text-2xl md:text-4xl lg:text-5xl font-bold">
+            خدماتنا
+          </h1>
+          <h1 className="text-dark-green text-2xl md:text-4xl lg:text-5xl font-bold">
+            الدقة تصنع الجودة، والخبرة فارق
+          </h1>
+        </section>
+
         <img
           src="/assets/Services/HeroSection/Right-Bubble.svg"
           className="absolute right-0 -top-24 z-10"
@@ -32,24 +41,24 @@ function HeroSection() {
           src="/assets/Services/HeroSection/Designing.svg"
           className="w-1/3 sm:w-1/3 xl:w-auto absolute right-16 bottom-24 md:bottom-2 lg:right-36 lg:bottom-8 z-0 "
         />
-        <div className="rotate-[15deg] w-[30%] md:w-1/4 xl:w-auto absolute left-10 top-12 md:top-2 lg:left-24 lg:top-24 z-0">
-          <p></p>
-          <img src={`/assets/Services/HeroSection/Planning.svg`} className="" />
-        </div>
+        <section className="rotate-[15deg] w-[30%] md:w-1/4 xl:w-auto absolute left-10 top-24 lg:left-24 lg:top-28 z-0">
+          {isText && (
+            <div className="absolute -top-12 sm:-top-16 md:-top-20 flex justify-between items-center gap-2 sm:gap-3 lg:gap-4 w-1/4 sm:w-1/3 md:w-auto">
+              <img src="/assets/Services/HeroSection/Small-Logo.svg" />
+              <p className="text-secondary text-xl md:text-2xl">التخطيط</p>
+              <img src="/assets/Services/HeroSection/Small-Logo.svg" />
+            </div>
+          )}
+          <img
+            src={`/assets/Services/HeroSection/Planning.svg`}
+            className={`${!isText && "bg-primary/80"}`}
+          />
+        </section>
 
         <img
           src="/assets/Services/HeroSection/Development.svg"
           className="w-1/3 sm:w-1/3 xl:w-auto absolute left-16 bottom-4 md:bottom-2 lg:left-36 lg:-bottom-4 z-0 "
         />
-
-        <div className="text-center mb-12 z-20">
-          <h1 className="text-primary text-2xl md:text-4xl lg:text-5xl font-bold">
-            خدماتنا
-          </h1>
-          <h1 className="text-dark-green text-2xl md:text-4xl lg:text-5xl font-bold">
-            الدقة تصنع الجودة، والخبرة فارق
-          </h1>
-        </div>
       </div>
     </div>
   );
