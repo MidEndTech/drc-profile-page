ad590ab88dba264912196c3bd64b7b99ecd15665
Musab Alsayed <musab20151@gmail.com>
2024-08-13 19:28:12 +0300

    Done

--- src/AboutUs/WorkTeamComponents/TeamCards.jsx
+++ src/AboutUs/WorkTeamComponents/TeamCards.jsx
@@ -4,7 +4,7 @@ function TeamCards(props) {
 
   return (
     <div>
-      <div key={props.index} className="z-10">
+      <div key={props.index} className="z-0">
         <div  className="flex-col justify-center items-center text-center">
           <div className="flex justify-center items-center mt-10 mb-10">
             <img
@@ -23,12 +23,12 @@ function TeamCards(props) {
 
           <div className="flex justify-center items-center  z-10 relative">
             <div
-              className="border-2 rounded-full p-1 absolute top-1"
-              style={{ borderColor: "#D9D9D9" }}
+              className={`border-2 rounded-full p-1 absolute top-1 ${props.borderStyle}`}
+              // style={{ borderColor: "#D9D9D9" }}
             >
               <div
-                className="p-4 rounded-full"
-                style={{ backgroundColor: "#D9D9D9" }}
+                className={`p-4 rounded-full ${props.bakgroundStyle}`}
+                // style={{ backgroundColor: "#D9D9D9" }}
               ></div>
             </div>
           </div>
