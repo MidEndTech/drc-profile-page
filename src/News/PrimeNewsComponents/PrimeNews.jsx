70a95ca77729627814d6b801755f48f30b850aee
Omir <omirabbasnw@gmail.com>
2024-08-14 14:20:55 +0300

    save work

---
+++ src/News/PrimeNewsComponents/PrimeNews.jsx
@@ -0,0 +1,5 @@
+function PrimeNews() {
+  return <div className="mt-24"></div>;
+}
+
+export default PrimeNews;
--- src/Pages/NewsPage.jsx
+++ src/Pages/NewsPage.jsx
@@ -1,9 +1,13 @@
 import HeroSection from "../News/HeroComponents/HeroSection";
+import PrimeNews from "../News/PrimeNewsComponents/PrimeNews";
 
 function NewsPage() {
   return (
-    <div>
+    <div className="bg-[#F5F5F5]">
       <HeroSection />
+      <div className="w-10/12 mx-auto">
+        <PrimeNews />
+      </div>
     </div>
   );
 }
