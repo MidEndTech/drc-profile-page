74a370e4ab3385fffbb3da284c72ba26fd5ec94e
Omir <omirabbasnw@gmail.com>
2024-08-14 11:01:59 +0300

    Create Hero Section in News Page

---
+++ src/News/HeroComponents/HeroSection.jsx
@@ -0,0 +1,18 @@
+import Heading from "../../SharedComponents/Heading";
+
+function HeroSection() {
+  return (
+    <div className="overflow-hidden">
+      <div className="w-screen h-screen flex flex-col items-center justify-center bg-[url('/assets/News/HeroSection/News-Hero.svg')] bg-cover bg-center">
+        <Heading className="text-white">أخبار دار الرحمة</Heading>
+        <input
+          type="text"
+          placeholder="ابحث في اخر الاخبار ..."
+          className="rounded-full p-2 md:pl-10 pr-6 shadow shadow-black/25 border-none outline-0 focus:outline-0"
+        />
+      </div>
+    </div>
+  );
+}
+
+export default HeroSection;
