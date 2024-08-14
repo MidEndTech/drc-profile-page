e4cfaef08a3cd65689714428d23a273d7e8d8f08
mubals442 <musab20151@gmail.com>
2024-08-14 14:00:35 +0300

    server slider

--- src/Services/ServicesSliderComponenets/CardInfo.jsx
+++ src/Services/ServicesSliderComponenets/CardInfo.jsx
@@ -1,26 +1,53 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { AnimatePresence, motion } from "framer-motion";
+import cardInfoObj from "./servicesCardInfo.json";
+import logoOpacity from "/assets/Services/ServicesSliderSection/logoOpacity.svg";
 
 function CardInfo(props) {
-  return (
-      <>
-        <div
-          key={props.index}
-          className="md:w-[515.83px] md:h-[586.52px] relative top-50"
-        >
-          <img
-            src={props.card}
-            alt=""
-            className="w-[330px] h-[480px] md:w-[390px] md:h-[492px]  rounded-[30px] rotate-[3deg]"
-          />
-        </div>
+  // Assuming cardInfoObj is an array and you're accessing the item by index
+  const [cardOfInfo, setCardOfInfo] = useState(cardInfoObj[props.index] || {});
 
-        <div className="w-[340px] h-[200px] mx-[10%] md:w-[641.4px] md:h-[390.82px] md:relative top-[100px] md:mx-[55px] text-justify">
-          <h1 className="text-[#223A39] font-[700] text-[33px] md:text-[36px]">
-            {props.titleOfCard}
-          </h1>
-          <p className="md:w-[597.79px]  md:h-[301.74px] font-[400] md:text-[24px] text-[20px]">{props.textOfCard}</p>
-        </div>
-      </>
+  return (
+    <>
+      <div className="overflow-hidden pt-20 pr-3 pb-3 pl-2">
+        <AnimatePresence mode="wait">
+          <motion.div
+            key={props.index}
+            initial={{ y: -600, opacity: 0 }}
+            animate={{ y: 0, opacity: 1 }}
+            exit={{ y: 1000, opacity: 0 }}
+            transition={{ delay: 0.2, duration: 1 }} // Added transition for smoothness
+            className="md:w-[515.83px] md:h-[586.52px]"
+          >
+            <img
+              src={cardOfInfo.card}
+              alt=""
+              className="w-[230px] h-[380px] md:w-[390px] md:h-[492px] rounded-[30px] rotate-[3deg]"
+            />
+          </motion.div>
+        </AnimatePresence>
+      </div>
+
+      <div className="overflow-hidden p-3 pb-20">
+        <AnimatePresence>
+          <motion.div
+            key={props.index}
+            initial={{ opacity: 0 }}
+            animate={{ y: 0, opacity: 1 }}
+            exit={{ opacity: 0 }}
+            transition={{ duration: 0.5 }} // Added transition for smoothness
+            className="w-[340px] h-[200px] md:w-[641.4px] md:h-[390.82px] md:relative top-[100px] md:mx-[55px] "
+          >
+            <h1 className="text-[#223A39] font-[700] text-[28px] md:text-[36px]">
+              {cardOfInfo.titleOfCard}
+            </h1>
+            <p className="md:w-[597.79px] md:h-[301.74px] font-[400] md:text-[24px] text-[20px] text-justify">
+              {cardOfInfo.textOfCard}
+            </p>
+          </motion.div>
+        </AnimatePresence>
+      </div>
+    </>
   );
 }
 
--- src/Services/ServicesSliderComponenets/ServicesSliderContainer.jsx
+++ src/Services/ServicesSliderComponenets/ServicesSliderContainer.jsx
@@ -1,44 +1,45 @@
 import React, { useState } from "react";
-import { useHover } from "@uidotdev/usehooks";
 import right from "/assets/Home/AssociationSection/Variant5.svg";
 import left from "/assets/Home/AssociationSection/Variant6.svg";
-
-import cardInfoObj from "./servicesCardInfo.json";
 import CardInfo from "./CardInfo";
-
-
+import blueOrange from "/assets/Services/ServicesSliderSection/BackgroundOrangeS.svg"
 
 function ServicesSliderContainer() {
-  const [cardOfInfo, setCardOfInfo] = useState([...cardInfoObj]);
-
+  const [indexCounter, setIndexCounter] = useState(0);
   return (
     <>
-      <div className="mt-10 lg:mt-20 flex flex-col items-center md:flex-row-reverse md:justify-center md:items-center">
-        {cardOfInfo.map((el, index) => {
-          return (
-            <CardInfo
-              index={index}
-              card={el.card}
-              titleOfCard={el.titleOfCard}
-              textOfCard={el.textOfCard}
-            />
-          );
-        })}
-
-        
+      <div
+        key={indexCounter}
+        className=" mt-10 lg:mt-20 flex flex-col items-center lg:flex-row-reverse lg:justify-center lg:items-center"
+      >
+        <CardInfo index={indexCounter} />
       </div>
 
-      <div className="flex justify-between mx-2 relative hover:opacity-4 bottom-[460px] lg:justify-between lg:bottom-[360px] lg:mx-[5%]">
+      <div className="flex justify-between mx-2 relative hover:opacity-4 bottom-[550px] lg:justify-between lg:bottom-[360px] lg:mx-[5%]">
         <img
+          onClick={() => {
+            setIndexCounter((prev) => prev + 1);
+          }}
           src={right}
           alt=""
-          className="cursor-pointer hover:opacity-4 w-12"
+          className={`z-10 cursor-pointer hover:opacity-4 w-12 ${
+            indexCounter === 7 && "invisible"
+          }`}
         />
         <img
+          onClick={() => {
+            setIndexCounter((prev) => prev - 1);
+          }}
           src={left}
           alt=""
-          className="cursor-pointer hover:opacity-4 w-12 lg:absolute lg:left-24"
+          className={`z-10 cursor-pointer hover:opacity-4 w-12 lg:absolute lg:left-20 ${
+            indexCounter === 0 && "invisible"
+          }`}
         />
+      </div>
+
+      <div className="z-0 absolute left-0 top-[900px]">
+        <img src={blueOrange} alt="" />
       </div>
     </>
   );
