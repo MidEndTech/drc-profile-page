e14c97009c3ddc154a710af82c5606376bbf70fd
Omir <omirabbasnw@gmail.com>
2024-08-17 18:00:36 +0300

    add a name to the page

--- src/Pages/NewsPage.jsx
+++ src/Pages/NewsPage.jsx
@@ -1,7 +1,11 @@
 import { Outlet } from "react-router";
 import HeroSection from "../News/HeroComponents/HeroSection";
+import { useEffect } from "react";
 
 function NewsPage() {
+  useEffect(() => {
+    document.title = "أخبارنا";
+  }, []);
   return (
     <div className="bg-[#F5F5F5] pb-24">
       <HeroSection />
