7250b065d2bc485287a09fa39bbd2fa6718b0974
Feisal14 <fisalgemar@gmail.com>
2024-08-18 15:38:33 +0300

    hoooooooya

--- src/Projects/NetwrokComponents/Network.jsx
+++ src/Projects/NetwrokComponents/Network.jsx
@@ -1,149 +1,172 @@
-import React from "react";
-import { useState } from "react";
-import NetworkDetails from "./NetworkDetails";
-import data from "./data.json";
-import { FaArrowLeft, FaArrowRight } from "react-icons/fa";
-import "./style.css";
+  import React from "react";
+  import { useState } from "react";
+  import NetworkDetails from "./NetworkDetails";
+  import data from "./data.json";
+  import { FaArrowLeft, FaArrowRight } from "react-icons/fa";
+  import "./style.css";
+  import { useNavigate } from "react-router";
 
-function Network() {
-  const [selected, setSelected] = useState(null);
+  function Network() {
+    const [selected, setSelected] = useState(null);
 
-  const handleClick = (key) => {
-    setSelected(data[key]);
-  };
+    const navigate = useNavigate();
 
-  return (
-    <>
-      {selected ? (
-        <NetworkDetails
-          image={selected.image}
-          heading={selected.heading}
-          text={selected.text}
-        />
-      ) : (
-        <div className="grid grid-cols-5  gap-2 md:gap-4 p-2 lg:grid-cols-5 justify-items-center ">
-          <div className="relative w-full col-start-2">
-            <img src={data["TopRight"].image} className="w-full" />
-            <div
-              className="absolute  cursor-pointer inset-0 bg-dark-green/80 opacity-0 hover:opacity-100 transition-opacity duration-300 ease-in-out rounded-3xl"
-              onClick={() => handleClick("TopRight")}
-            >
-              {" "}
-              <FaArrowLeft
-                size={30}
-                className="text-white absolute border-2 p-1 border-white rounded-full flex items-start  top-10  left-10 "
-              />
-            </div>
-          </div>
-          <div className="relative w-full">
-            <img src={data["TopMiddle"].image} className="w-full " />
-            <div
-              onClick={() => handleClick("TopMiddle")}
-              className="absolute inset-0 rounded-3xl cursor-pointer bg-dark-green/80 opacity-0 hover:opacity-100 transition-opacity duration-300 ease-in-out"
-            >
-              {" "}
-              <FaArrowLeft
-                size={30}
-                className="text-white absolute border-2 p-1 border-white rounded-full flex items-start  top-10  left-10 "
-              />
-            </div>
-          </div>
+    const handleClick = (key) => {
+      const project = data[key];
+      setSelected(project);
+      navigate(`/projects/${project.id}`);
 
-          <div className="relative w-full row-start-1 row-end-3 col-start-4">
-            <img
-              src={data["TopLeft"].image}
-              className="w-full "
-              alt={data["TopLeft"].heading}
-            />
-            <div
-              onClick={() => handleClick("TopLeft")}
-              className="absolute rounded-3xl cursor-pointer inset-0 bg-dark-green/80 opacity-0 hover:opacity-100 transition-opacity duration-300 ease-in-out"
-            >
-              {" "}
-              <FaArrowLeft
-                size={30}
-                className="text-white absolute border-2 p-1 border-white rounded-full flex items-start  top-10  left-10 "
-              />
-            </div>
-          </div>
-          <div className="relative w-full  col-span-2 row-start-2 row-end-4 col-start-2">
-            <img src={data["Center"].image} className="w-full " />
+    };
 
-            <div
-              onClick={() => handleClick("Center")}
-              className="absolute cursor-pointer rounded-3xl inset-0 bg-dark-green/80 opacity-0 hover:opacity-100 transition-opacity duration-300 ease-in-out"
-            >
-              <FaArrowLeft
-                size={30}
-                className="text-white absolute border-2 p-1 border-white rounded-full flex items-start  top-10  left-10 "
-              />
+    return (
+      <>
+        {selected ? (
+          <NetworkDetails
+            image={selected.image}
+            heading={selected.heading}
+            text={selected.text}
+          />
+        ) : (
+          <div className="grid grid-cols-5  gap-2 md:gap-4 p-2 lg:grid-cols-5 justify-items-center ">
+            <div className="relative w-full col-start-2">
+              <img src={data["TopRight"].image} className="w-full" />
+              <div
+                className="absolute  cursor-pointer inset-0 hover:bg-dark-green/80 opacity-100 md:opacity-0 hover:opacity-100 transition-opacity duration-300 ease-in-out rounded-3xl"
+                onClick={() => handleClick("TopRight")}
+              >
+                <FaArrowLeft className="text-white absolute border md:border-2 p-1 border-white rounded-full flex items-start md:top-10 md:left-10 md:text-[30px] left-2 top-1  text-[15px] " />
+              </div>
+              <div className="absolute bottom-2 right-1 md:right-5 text-white font-bold">
+                <span className="block text-[4px] sm:text-[10px] md:text-[13px] md:p-1 lg:text-[16px] ">
+                  محافظة الجموم
+                </span>
+                <span className="block text-[5px] sm:text-[13px] md:text-[13px] lg:text-[18px]  \">
+                  مجمعات الخدمات الحكومية
+                </span>
+              </div>
             </div>
-          </div>
-          <div className="relative w-full col-start-4 row-start-3 row-end-5">
-            <img
-              src={data["MiddleLeft"].image}
-              className="w-full h-full object-cover rounded-3xl"
-            />
-            <div
-              onClick={() => handleClick("MiddleLeft")}
-              className="absolute inset-0 cursor-pointer bg-dark-green/80 opacity-0 hover:opacity-100 transition-opacity duration-300 ease-in-out rounded-3xl"
-            >
-              {" "}
-              <FaArrowLeft
-                size={30}
-                className="text-white absolute border-2 p-1 border-white rounded-full flex items-start  top-10  left-10 "
-              />
+            <div className="relative w-full">
+              <img src={data["TopMiddle"].image} className="w-full " />
+              <div
+                onClick={() => handleClick("TopMiddle")}
+                className="absolute  cursor-pointer inset-0 hover:bg-dark-green/80 opacity-100 md:opacity-0 hover:opacity-100 transition-opacity duration-300 ease-in-out rounded-3xl"
+              >
+                {" "}
+                <FaArrowLeft className="text-white absolute border md:border-2 p-1 border-white rounded-full flex items-start md:top-10 md:left-10 md:text-[30px] left-2 top-1  text-[15px] " />
+              </div>
+              <div className="absolute bottom-2 right-1 md:right-5 text-white font-bold">
+                <span className="block text-[4px] sm:text-[10px] md:text-[13px] md:p-1 lg:text-[16px] ">
+                  مركز الترحيب{" "}
+                </span>
+                <span className="block text-[5px] sm:text-[13px] md:text-[13px] lg:text-[18px]  \">
+                  بالمدينة المنورة{" "}
+                </span>
+              </div>
             </div>
-          </div>
 
-          <div className="relative w-full col-start-2 row-start-4 row-end-6">
-            <img
-              src={data["BottomRight"].image}
-              className="w-full h-full object-cover rounded-3xl"
-            />
-            <div
-              onClick={() => handleClick("BottomRight")}
-              className="absolute inset-0 rounded-3xl cursor-pointer bg-dark-green/80 opacity-0 hover:opacity-100 transition-opacity duration-300 ease-in-out"
-            >
-              {" "}
-              <FaArrowLeft
-                size={30}
-                className="text-white absolute border-2 p-1 border-white rounded-full flex items-start  top-10  left-10 "
+            <div className="relative w-full row-start-1 row-end-3 col-start-4">
+              <img src={data["TopLeft"].image} className="w-full " />
+              <div
+                onClick={() => handleClick("TopLeft")}
+                className="absolute  cursor-pointer inset-0 hover:bg-dark-green/80 opacity-100 md:opacity-0 hover:opacity-100 transition-opacity duration-300 ease-in-out rounded-3xl"
+              >
+                {" "}
+                <FaArrowLeft className="text-white absolute border md:border-2 p-1 border-white rounded-full flex items-start md:top-10 md:left-10 md:text-[30px] left-2 top-1  text-[15px] " />
+              </div>
+              <div className="absolute bottom-2 right-1 md:right-5 text-white font-bold">
+                <span className="block text-[6px] sm:text-[10px] md:text-[17px] md:p-1 lg:text-[25px] ">
+                  فندق البدر
+                </span>
+              </div>
+            </div>
+            <div className="relative w-full  col-span-2 row-start-2 row-end-4 col-start-2">
+              <img src={data["Center"].image} className="w-full " />
+              <div
+                onClick={() => handleClick("Center")}
+                className="absolute  cursor-pointer inset-0 hover:bg-dark-green/80 opacity-100 md:opacity-0 hover:opacity-100 transition-opacity duration-300 ease-in-out rounded-3xl"
+              >
+                {" "}
+                <FaArrowLeft className="text-white absolute border md:border-2 p-1 border-white rounded-full flex items-start md:top-10 md:left-10 md:text-[30px] left-2 top-1  text-[15px] " />
+              </div>
+              <div className="absolute bottom-2 right-1 md:right-5 text-white font-bold">
+                <span className="block text-[7px] sm:text-[10px] md:text-[17px] md:p-1 lg:text-[30px] ">
+                  جامع العباس
+                </span>
+              </div>
+            </div>
+            <div className="relative w-full col-start-4 row-start-3 row-end-5">
+              <img
+                src={data["MiddleLeft"].image}
+                className="w-full h-full object-cover rounded-3xl"
               />
+              <div
+                onClick={() => handleClick("MiddleLeft")}
+                className="absolute  cursor-pointer inset-0 hover:bg-dark-green/80 opacity-100 md:opacity-0 hover:opacity-100 transition-opacity duration-300 ease-in-out rounded-3xl"
+              >
+                {" "}
+                <FaArrowLeft className="text-white absolute border md:border-2 p-1 border-white rounded-full flex items-start md:top-10 md:left-10 md:text-[30px] left-2 top-1  text-[15px] " />
+              </div>
+              <div className="absolute bottom-2 right-1 md:right-5 text-white font-bold">
+                <span className="block text-[7px] sm:text-[10px] md:text-[17px] md:p-1 lg:text-[20px] ">
+                  عنوان المشروع الخامس{" "}
+                </span>
+              </div>
+            </div>
+
+            <div className="relative w-full col-start-2 row-start-4 row-end-6">
+              <img
+                src={data["BottomRight"].image}
+                className="w-full h-full object-cover rounded-3xl"
+              />
+              <div
+                onClick={() => handleClick("BottomRight")}
+                className="absolute  cursor-pointer inset-0 hover:bg-dark-green/80 opacity-100 md:opacity-0 hover:opacity-100 transition-opacity duration-300 ease-in-out rounded-3xl"
+              >
+                {" "}
+                <FaArrowLeft className="text-white absolute border md:border-2 p-1 border-white rounded-full flex items-start md:top-10 md:left-10 md:text-[30px] left-2 top-1  text-[15px] " />
+              </div>
+              <div className="absolute bottom-2 right-1 md:right-5 text-white font-bold">
+                <span className="block text-[7px] sm:text-[10px] md:text-[17px] md:p-1 lg:text-[25px] ">
+                  فندق دار القصبي
+                </span>
+              </div>
+            </div>
+            <div className="relative w-full row-start-4 row-end-6 col-start-3">
+              <img
+                src={data["BottomCenter"].image}
+                className="w-full h-full object-cover rounded-3xl"
+              />
+              <div
+                onClick={() => handleClick("BottomCenter")}
+                className="absolute  cursor-pointer inset-0 hover:bg-dark-green/80 opacity-100 md:opacity-0 hover:opacity-100 transition-opacity duration-300 ease-in-out rounded-3xl"
+              >
+                {" "}
+                <FaArrowLeft className="text-white absolute border md:border-2 p-1 border-white rounded-full flex items-start md:top-10 md:left-10 md:text-[30px] left-2 top-1  text-[15px] " />
+              </div>
+              <div className="absolute bottom-2 right-1 md:right-5 text-white font-bold">
+                <span className="block text-[7px] sm:text-[10px] md:text-[17px] md:p-1 lg:text-[20px] ">
+                  فيلا سكنيه خاصة{" "}
+                </span>
+              </div>
+            </div>
+            <div className="relative w-full col-start-4 row-start-5 row-end-5 ">
+              <img src={data["BottomLeft"].image} className="w-full h-full" />
+              <div
+                onClick={() => handleClick("BottomLeft")}
+                className="absolute  cursor-pointer inset-0 hover:bg-dark-green/80 opacity-100 md:opacity-0 hover:opacity-100 transition-opacity duration-300 ease-in-out rounded-3xl"
+              >
+                {" "}
+                <FaArrowLeft className="text-white absolute border md:border-2 p-1 border-white rounded-full flex items-start md:top-10 md:left-10 md:text-[30px] left-2 top-1  text-[15px] " />
+              </div>
+              <div className="absolute bottom-2 right-1 md:right-5 text-white font-bold">
+                <span className="block text-[7px] sm:text-[10px] md:text-[17px] md:p-1 lg:text-[20px] ">
+                  حديقة الحمراء
+                </span>
+              </div>
             </div>
           </div>
-          <div className="relative w-full row-start-4 row-end-6 col-start-3">
-            <img
-              src={data["BottomCenter"].image}
-              className="w-full h-full object-cover rounded-3xl"
-            />
-            <div
-              className="absolute inset-0 rounded-3xl bg-dark-green/80 opacity-0 hover:opacity-100 transition-opacity duration-300 ease-in-out cursor-pointer"
-              onClick={() => handleClick("BottomCenter")}
-            >
-              {" "}
-              <FaArrowLeft
-                size={30}
-                className="text-white absolute border-2 p-1 border-white rounded-full flex items-start  top-10  left-10 "
-              />
-            </div>
-          </div>
-          <div className="relative w-full col-start-4 row-start-5 row-end-5 ">
-            <img src={data["BottomLeft"].image} className="w-full h-full" />
-            <div
-              onClick={() => handleClick("BottomLeft")}
-              className="absolute cursor-pointer rounded-3xl inset-0 bg-dark-green/80 opacity-0 hover:opacity-100 transition-opacity duration-300 ease-in-out"
-            >
-              {" "}
-              <FaArrowLeft
-                size={30}
-                className="text-white absolute border-2 p-1 border-white rounded-full flex items-start  top-10  left-10 "
-              />
-            </div>
-          </div>
-        </div>
-      )}
-    </>
-  );
-}
-export default Network;
+        )}
+      </>
+    );
+  }
+  export default Network;
--- src/Projects/NetwrokComponents/NetworkDetails.jsx
+++ src/Projects/NetwrokComponents/NetworkDetails.jsx
@@ -1,14 +1,23 @@
 import React from "react";
+import { useParams } from "react-router-dom";
+import data from "./data.json";
 import Heading from "../../SharedComponents/Heading";
 import Paragraph from "../../SharedComponents/Paragraph";
 
-function NetworkDetails({ image, heading, text }) {
+function NetworkDetails() {
+  const { id } = useParams();
+  const project = Object.values(data).find((proj) => proj.id.toString() === id);
+
+  if (!project) {
+    return <div>Project not found</div>;
+  }
+
   return (
-    <div className="p-10 flex flex-col text-center items-center">
-      <img src={image} alt={heading} className="w-[100px]}" />
-      <Heading className=" text-primary p-10 ">{heading}</Heading>
-      <Paragraph className="text-dark-green text-lg text-justify px-20 ">
-        {text}
+    <div className="p-10 flex flex-col text-center items-center mt-24">
+      <img src={project.image} alt={project.heading} className="" />
+      <Heading className="text-primary p-10">{project.heading}</Heading>
+      <Paragraph className="text-dark-green text-lg text-justify px-20">
+        {project.text}
       </Paragraph>
     </div>
   );
