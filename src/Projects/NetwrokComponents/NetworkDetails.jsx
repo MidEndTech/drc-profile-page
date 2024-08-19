407b29bfe466b1f767b944630c509b2bee5b6bf7
Feisal14 <fisalgemar@gmail.com>
2024-08-19 09:14:32 +0300

    Added the herossection

--- src/Projects/NetwrokComponents/NetworkDetails.jsx
+++ src/Projects/NetwrokComponents/NetworkDetails.jsx
@@ -3,6 +3,7 @@ import { useParams } from "react-router-dom";
 import data from "./data.json";
 import Heading from "../../SharedComponents/Heading";
 import Paragraph from "../../SharedComponents/Paragraph";
+import HeroSection from "../HeroComponents/HeroSection";
 
 function NetworkDetails() {
   const { id } = useParams();
@@ -13,13 +14,17 @@ function NetworkDetails() {
   }
 
   return (
-    <div className="p-10 flex flex-col text-center items-center mt-24">
-      <img src={project.image} alt={project.heading} className="" />
-      <Heading className="text-primary p-10">{project.heading}</Heading>
-      <Paragraph className="text-dark-green text-lg text-justify px-20">
-        {project.text}
-      </Paragraph>
-    </div>
+    <>
+      {" "}
+      <HeroSection />
+      <div className="p-10 flex flex-col text-center items-center mt-24">
+        <img src={project.image} alt={project.heading} className="" />
+        <Heading className="text-primary p-10">{project.heading}</Heading>
+        <Paragraph className="text-dark-green text-lg text-justify px-20">
+          {project.text}
+        </Paragraph>
+      </div>
+    </>
   );
 }
 
