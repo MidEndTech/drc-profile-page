a6a9b1c3e81709d35961d4bd8fa7ff12afc430fd
Musab Alsayed <musab20151@gmail.com>
2024-08-20 07:58:23 +0300

    contact us the input return initial aftar send and responseve the note

--- src/ContactUs/ContactForm.jsx
+++ src/ContactUs/ContactForm.jsx
@@ -1,4 +1,4 @@
-import React, { useRef } from "react";
+import React, { useReducer, useRef } from "react";
 import emailjs from "@emailjs/browser";
 import Label from "../SharedComponents/Label";
 
@@ -6,10 +6,46 @@ import { ToastContainer, toast } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 
 function ContactForm() {
+  // THIS FORM FOR THE EMAIL.JS PUACKIG
   const form = useRef();
 
-  const sendEmail = (e) => {
+  //THE INITIAL VALUES OF FORMS INPUT
+  const initialState = {
+    user_name: "",
+    user_email: "",
+    user_message_title: "",
+    user_message: "",
+  };
 
+  //REDUCER FUNCTIONS FOR REDUCER HOOK
+  const reducer = (state, action) => {
+    switch (action.type) {
+      case "input":
+        return { ...state, [action.field]: action.value };
+      case "reset":
+        return initialState;
+      default:
+        return state;
+    }
+  };
+
+  const [state, dispatch] = useReducer(reducer, initialState);
+
+  //THIS HANDLE TO MAKE TYPE IN THE INPUT
+  const handleChange = (e) => {
+    dispatch({
+      type: "input",
+      field: e.target.name,
+      value: e.target.value,
+    });
+  };
+
+  //THIS FUNCTIONS FOR ON SUBMINT THE INPUT REST AND RETURN INITIAL VALUE
+  const handleReset = () => {
+    dispatch({ type: "reset" }); // Use the correct function name to reset the form state
+  };
+
+  const sendEmail = (e) => {
     e.preventDefault();
 
     const notifySuccess = () => toast.success("Success!");
@@ -17,21 +53,22 @@ function ContactForm() {
 
     emailjs
       .sendForm("service_yry6fox", "template_s4uawbj", form.current, {
-        publicKey: "bPbqjyFjfjDLvLNQZ",
+        publicKey: "bPbqjyFjfjDLvLNQ",
       })
       .then(
         () => {
-          notifySuccess()
+          notifySuccess();
+          handleReset();
         },
-        (error) => {
-          notifyErorr()
+        () => {
+          notifyErorr();
         }
       );
   };
 
   return (
     <div className="lg:ml-[2%] pt-5 lg:pt-0">
-              <ToastContainer />
+        <ToastContainer className={"w-[290px]"} />
 
       <form ref={form} onSubmit={sendEmail}>
         <div className="flex flex-col justify ">
@@ -40,9 +77,9 @@ function ContactForm() {
             className="font-[400] text-[18px] mx-[2%] h-[55px] md:mx-0 md:w-[742px] md:h-[71px] rounded-[32px] shadow-lg shadow-[#00000026] border-none pr-5"
             type="text"
             name="user_name"
-            id=""
+            value={state.user_name}
+            onChange={handleChange}
             placeholder="الإسم الكامل"
-            required
           />
         </div>
 
@@ -53,9 +90,9 @@ function ContactForm() {
             className="font-[400] text-[18px] mx-[2%] h-[55px] md:mx-0  md:w-[742px] md:h-[71px] rounded-[32px] shadow-md shadow-[#00000026] border-none pr-5"
             type="text"
             name="user_email"
-            id=""
+            value={state.user_email}
+            onChange={handleChange}
             placeholder="البريد الإلكتروني"
-            required
           />
         </div>
 
@@ -66,9 +103,9 @@ function ContactForm() {
             className="font-[400] text-[18px] mx-[2%] h-[55px] md:mx-0  md:w-[742px] md:h-[71px] rounded-[32px] shadow-md shadow-[#00000026] border-none pr-5"
             type="text"
             name="user_message_title"
-            id=""
+            value={state.user_message_title}
+            onChange={handleChange}
             placeholder="عنوان الرسالة"
-            required
           />
         </div>
 
@@ -77,14 +114,15 @@ function ContactForm() {
           <textarea
             className="pt-3 font-[400] text-[18px] mx-[2%] md:mx-0 h-[190px] md:w-[742px] md:h-[254px] rounded-[32px] shadow-md shadow-[#00000026] border-none pr-5"
             name="user_message"
-            id=""
+            value={state.user_message}
+            onChange={handleChange}
             placeholder="الرسالة..."
-            required
           ></textarea>
         </div>
 
         <div className="my-7 mx-4 flex justify-center md:flex md:justify-start lg:mr-[3%]">
           <button
+            type="submit"
             className="border w-[180px] h-[59px] rounded-[64px] bg-[#9D6636] hover:bg-[#8a592e] text-[#FFFFFF] font-[700] text-[20px]"
           >
             ارسال
