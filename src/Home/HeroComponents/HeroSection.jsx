76fec81a024cff635df1fccc036d2b15050178fb
Omir <omirabbasnw@gmail.com>
2024-08-21 14:01:21 +0300

    Save Work

--- src/Home/HeroComponents/HeroSection.jsx
+++ src/Home/HeroComponents/HeroSection.jsx
@@ -1,6 +1,6 @@
+import "./hero.css";
 import data from "./field.json";
 import { useRef, useState, useEffect } from "react";
-import HeroContainer from "./HeroContainer";
 import HeroInfo from "./HeroInfo";
 import HeroCarousel from "./HeroCarousel";
 import Arrows from "./Arrows";
@@ -37,7 +37,24 @@ function HeroSection() {
 
   return (
     <main className="overflow-hidden">
-      <HeroContainer currentIndex={currentIndex}>
+      <div className="relative h-screen w-screen flex flex-col items-center justify-center ">
+        {data.map((item, index) => {
+          return (
+            <img
+              key={index}
+              src={item.bigPicture}
+              className={`${
+                currentIndex === index
+                  ? `block ${
+                      currentIndex % 2 === 0
+                        ? "kenburns-top-right"
+                        : "kenburns-top-right-reverse"
+                    } `
+                  : "hidden"
+              } absolute inset-0 w-full h-full object-cover object-center`}
+            />
+          );
+        })}
         <div className="flex flex-col justify-end lg:flex-row gap-4 xl:gap-0 lg:justify-between items-center w-full">
           <HeroInfo currentIndex={currentIndex} isNext={isNext} />
           <div className="flex flex-col pr-4 sm:p-0 w-full sm:w-1/2 2xl:w-1/3">
@@ -49,7 +66,7 @@ function HeroSection() {
             />
           </div>
         </div>
-      </HeroContainer>
+      </div>
     </main>
   );
 }
