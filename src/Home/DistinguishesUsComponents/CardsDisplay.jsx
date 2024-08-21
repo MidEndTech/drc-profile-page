f9a849110e5b53b4a2ce54fbfd219cb1860ef8b0
mubals442 <musab20151@gmail.com>
2024-08-21 14:27:42 +0300

    to pc animitions

--- src/Home/DistinguishesUsComponents/CardsDisplay.jsx
+++ src/Home/DistinguishesUsComponents/CardsDisplay.jsx
@@ -3,12 +3,13 @@ import { motion } from "framer-motion";
 import { AnimatePresence } from "framer-motion";
 
 function CardsDisplay(props) {
+
   return (
     <AnimatePresence>
       <motion.div
-        initial={{ y: 0, x: 0, opacity: 0 }}
-        animate={{ y: 0, x: 0, opacity: 1 }}
-        exit={{ y: 0, x: 0, opacity: 0 }}
+        initial={{ y: -200, x: 50, opacity: 0 }}
+        animate={{ y: props.lgTop, x:props.lgLeft, opacity: 1 }}
+        exit={{ y: -200, x: 50, opacity: 0 }}
         transition={{duration: 1 }}
         key={props.index}
       >
@@ -16,21 +17,21 @@ function CardsDisplay(props) {
           className={`animate-bounce z-10 shadow-md border-1 rounded-full flex justify-center items-center py-[6px] px-[20px] absolute`}
           style={{
             backgroundColor: `${props.backGroundColor}`,
-            top: `${
-              props.funcCond === "lg"
-                ? props.lgTop
-                : props.funcCond === "md"
-                ? props.mdTop
-                : props.top
-            }px`,
-            left: `${
-              props.funcCond === "lg"
-                ? props.lgLeft
-                : props.funcCond === "md"
-                ? props.mdLeft
-                : props.left
-            }px`,
-            right: `${props.funcCond === "phone" ? props.right : null}px`,
+            // top: `${
+            //   props.funcCond === "lg"
+            //     ? props.lgTop
+            //     : props.funcCond === "md"
+            //     ? props.mdTop
+            //     : props.top
+            // }px`,
+            // left: `${
+            //   props.funcCond === "lg"
+            //     ? props.lgLeft
+            //     : props.funcCond === "md"
+            //     ? props.mdLeft
+            //     : props.left
+            // }px`,
+            // right: `${props.funcCond === "phone" ? props.right : null}px`,
             rotate: `${props.rotation}`,
             animationDuration: `${props.duration}s`,
           }}
